d45931a5af6ea6d6518fde84467dd04fdfe7dbef
kuninaka <abc13732@icloud.com>
2019-11-29 16:52:06 +0900

    MySQLの使い方

--- thanks.php
+++ thanks.php
@@ -1,13 +1,18 @@
 <?php
+// 呼び込みの設定
 require_once('function.php');
-
+require_once('dbconnect.php');
+// post送信ではなかったら
 if($_SERVER["REQUEST_METHOD"] != "POST"){
     header('Location: index.php');
 };
-
+// 入力内容の取得
  $nickname = $_POST['nickname'];
  $email = $_POST['email'];
  $content = $_POST['content'];
+// 'INSERT INTO surveys (nickname, email, content) VALUES (?, ?, ?)'ここが使うときに変わる
+ $stmt = $dbh->prepare('INSERT INTO surveys (nickname, email, content) VALUES (?, ?, ?)');
+ $stmt->execute([$nickname, $email, $content]);//?を変数に置き換えてSQLを実行
 ?>
 <!DOCTYPE html>
 <html lang="ja">
---
+++ view.php
@@ -0,0 +1,31 @@
+<?php
+    require_once('function.php');
+    require_once('dbconnect.php');
+
+    //SQLを実行
+    // ①DBからデータを取得
+    $stmt = $dbh->prepare('SELECT * FROM surveys');
+    $stmt->execute();
+    $results = $stmt->fetchAll();
+    // $results = $stmt->fetchAll();この中身は下のようになっている
+    // [
+    //   0 => ['id' => 1, 'nicname' => 'atushi'..];
+    //   1 => ['id' => 2, 'nicname' => 'naoki'..];
+    //   2 => ['id' => 3, 'nicname' => 'mashimo'..];
+    // ];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>一覧</title>
+</head>
+<body>
+<!-- //②画面に表示する -->
+    <?php foreach ($results as $result): ?>
+        <p><?php echo h($result['nickname']); ?></p>
+        <p><?php echo h($result['email']); ?></p>
+        <p><?php echo h($result['content']); ?></p>
+    <?php endforeach; ?>
+</body>
+</html>
