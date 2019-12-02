a5f45cc383b5e6a1754eee87bbf40ee84644a63e
kuninaka <abc13732@icloud.com>
2019-12-02 16:54:49 +0900

    条件を限定した時の処理の完成

--- search.php
+++ search.php
@@ -11,34 +11,32 @@ require_once('dbconnect.php');
 
 // ユーザーが入力した内容を取得
 // isset() 引数が存在していれば
+$nickname = '';
 if(isset($_GET['nickname'])){
   $nickname = $_GET['nickname'];
-  echo($nickname);
+  // echo($nickname);
 }
 
 // ユーザーが入力した内容でDBを検索
   // DBに接続する。これは上でやっているため省略
 
   // MySQLを実行する
+  // WHERE を使って条件を絞る
   // prepare = 準備
-  $stmt = $dbh->prepare('SELECT * FROM surveys');
-  // execute = 実行する
-  $stmt->execute();
+  $stmt = $dbh->prepare('SELECT * FROM surveys WHERE nickname = ?');
+  // execute = 実行する   引数はお問い合わせ内容に入力したことのある内容
+  $stmt->execute([$nickname]);
   // 実行したけ結果を変数$resultsに代入する
   // 保存するときなどは下の処理はいらない
   $results = $stmt->fetchAll();
 
-  echo'<pre>';
-  var_dump($results);
-  exit;
+  // 検索結果の表示確認のため書いた
+  // echo'<pre>';
+  // var_dump($results);
+  // exit;
 
 
 // 検索結果を画面に表示する
-
-
-
-
-
 
 
 ?>
@@ -56,5 +54,10 @@ if(isset($_GET['nickname'])){
         <input type="submit" value="検索">
     </form>
     <!-- 画面に表示する -->
+    <?php foreach($results as $key ): ?>
+    <!-- idやnicknameはDBのカラム名 -->
+    <p> <?php echo $key['id'] ?> </p>
+    <p> <?php echo $key['nickname'] ?> </p>
+    <?php endforeach; ?>
 </body>
 </html>
